c1c0b815e4dba71c8e6f489e1a02c47ab28dec00
Dawn N Mathews <mathewsdawn09@gmail.com>
2020-07-22 16:12:04 -0400

    added examples for arrow functions with 0,1 or many arguments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Part 1 - Single or No Argument
 
 /* Single Argument*/
 // Simple function
@@ -11,3 +12,42 @@ console.log(sayHello1('naruto'));
 // Arrow / Fat Function
 let sayHello2 = name => 'hello ' + name;
 console.log(sayHello2('dora'));
+
+// Part 2
+
+let divideby2 = a => a/2;
+console.log(divideby2(6));
+
+// Preferred Syntax
+let divideby3 = (a) => { return a/3 };
+console.log(divideby3(6));
+
+// Part 3
+
+var pia = 3.14;
+let returnpia = () => { return pia };
+console.log(returnpia());
+
+// or
+
+var pib = 3.14;
+let returnpib = _ => { return pib };
+console.log(returnpib());
+
+// Part 4 - Two Arguments
+
+let greetperson = (greeting, name) => { return greeting+' '+name };
+console.log(greetperson('bonjour','max'));
+
+// Part 5 - N or more than 2 arguments
+
+var displaynames = (...n) => {
+  // console.log("Name 1 is : "+ n[0])
+  // console.log("Name 2 is : "+ n[1])
+  // console.log("Name 3 is : "+ n[2])
+  n.forEach((item, i) => {
+      console.log('Name at index '+i+' is '+item)
+  });
+
+}
+displaynames('name1','name2','name3');
